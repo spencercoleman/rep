9b52e48e2c2ce355948efc1cc72a6a9a4232b21f
spencercoleman <spencer@edc.vc>
2022-08-07 18:19:56 -0700

    Add signup logic and handle email/password validation

--- server/models/userModel.js
+++ server/models/userModel.js
@@ -1,4 +1,6 @@
 const mongoose = require('mongoose');
+const bcrypt = require('bcrypt');
+const validator = require('validator');
 
 const Schema = mongoose.Schema;
 
@@ -13,5 +15,36 @@ const userSchema = new Schema({
         required: true
     }
 });
+
+userSchema.statics.signup = async function(email, password) {
+    // Validate email and password
+    if (!email || !password) {
+        throw new Error('All fields must be filled');
+    }
+
+    if (!validator.isEmail(email)) {
+        throw new Error('Email is not valid');
+    }
+
+    if (!validator.isStrongPassword(password)) {
+        throw new Error('Password is too weak');
+    }
+
+    const exists = await this.findOne({ email });
+
+    if (exists) {
+        throw new Error('Email already in use');
+    }
+
+    const salt = await bcrypt.genSalt(10);
+    const hash = await bcrypt.hash(password, salt);
+
+    const user = await this.create({
+        email,
+        password: hash
+    });
+
+    return user;
+}
 
 module.exports = mongoose.model('User', userSchema);

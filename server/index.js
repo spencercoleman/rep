885da95e216cbdc999678aae51520512f7faf859
spencercoleman <spencer@edc.vc>
2022-08-09 14:23:59 -0700

    Resolve unexpected token

--- server/index.js
+++ server/index.js
@@ -12,7 +12,7 @@ const app = express();
 
 app.use(express.json());
 
-app.use(express.static(path.resolve(__dirname, './client/build')));
+app.use(express.static(path.resolve(__dirname, '../client/build')));
 
 app.use(logger('dev'));
 

fd8fc931ce759c3027c1f7bf8cb713312b6d82d7
spencercoleman <spencer@edc.vc>
2022-08-10 19:22:51 -0700

    Resolve missing auth context on workout form tests

--- client/src/components/tests/WorkoutForm.test.js
+++ client/src/components/tests/WorkoutForm.test.js
@@ -1,91 +1,98 @@
 import React from "react";
 import { render, screen } from "@testing-library/react";
-import { MemoryRouter } from 'react-router-dom'; 
+import { MemoryRouter } from "react-router-dom";
+import { AuthProvider } from "../../context/AuthContext";
 import { WorkoutsProvider } from "../../context/WorkoutsContext";
 import { ExercisesProvider } from "../../context/ExercisesContext";
 import "@testing-library/jest-dom";
-import WorkoutForm from '../WorkoutForm';
+import WorkoutForm from "../WorkoutForm";
 import userEvent from "@testing-library/user-event";
 
 const Wrapper = ({ children }) => {
-    return (
+  return (
+    <AuthProvider>
         <WorkoutsProvider>
-            <ExercisesProvider>
-                <MemoryRouter>
-                    {children}
-                </MemoryRouter>
-            </ExercisesProvider>
+        <ExercisesProvider>
+            <MemoryRouter>{children}</MemoryRouter>
+        </ExercisesProvider>
         </WorkoutsProvider>
+        </AuthProvider>
+  );
+};
+
+describe("Workout form component", () => {
+  it("renders a form", () => {
+    render(<WorkoutForm />, { wrapper: Wrapper });
+    expect(screen.getByRole("form")).toBeInTheDocument();
+  });
+
+  it("renders CTA to add a new workout", () => {
+    render(<WorkoutForm />, { wrapper: Wrapper });
+    expect(screen.getByText("Add a new workout")).toBeInTheDocument();
+  });
+
+  it("Renders an input for the workout title", () => {
+    render(<WorkoutForm />, { wrapper: Wrapper });
+    expect(
+      screen.getByPlaceholderText("Add a title to your workout")
+    ).toBeInTheDocument();
+  });
+
+  it("Renders an input for the workout notes", () => {
+    render(<WorkoutForm />, { wrapper: Wrapper });
+    expect(
+      screen.getByPlaceholderText("Add notes about your workout (optional)")
+    ).toBeInTheDocument();
+  });
+
+  it("Renders an input workout duration", () => {
+    render(<WorkoutForm />, { wrapper: Wrapper });
+    expect(screen.getByLabelText("Duration (minutes)")).toBeInTheDocument();
+    expect(screen.getByPlaceholderText("60")).toBeInTheDocument();
+  });
+
+  it("Renders an add exercise button", () => {
+    render(<WorkoutForm />, { wrapper: Wrapper });
+    expect(screen.getByText("Add an Exercise")).toBeInTheDocument();
+  });
+
+  it("Renders a submit button", () => {
+    render(<WorkoutForm />, { wrapper: Wrapper });
+    expect(screen.getByText("Finish")).toBeInTheDocument();
+  });
+});
+
+describe("Worokut title input", () => {
+  it("has correct value after user input", () => {
+    render(<WorkoutForm />, { wrapper: Wrapper });
+    const input = screen.getByPlaceholderText("Add a title to your workout");
+
+    userEvent.type(input, "Ring Fit");
+
+    expect(input).toHaveValue("Ring Fit");
+  });
+});
+
+describe("Worokut notes input", () => {
+  it("has correct value after user input", () => {
+    render(<WorkoutForm />, { wrapper: Wrapper });
+    const input = screen.getByPlaceholderText(
+      "Add notes about your workout (optional)"
     );
-}
 
-describe('Workout form component', () => {
-    it('renders a form', () => {
-        render(<WorkoutForm />, { wrapper: Wrapper });
-        expect(screen.getByRole('form')).toBeInTheDocument();
-    });
+    userEvent.type(input, "Worked out all night");
 
-    it('renders CTA to add a new workout', () => {
-        render(<WorkoutForm />, { wrapper: Wrapper });
-        expect(screen.getByText('Add a new workout')).toBeInTheDocument();
-    });
-    
-    it('Renders an input for the workout title', () => {
-        render(<WorkoutForm />, { wrapper: Wrapper });
-        expect(screen.getByPlaceholderText('Add a title to your workout')).toBeInTheDocument();
-    });
-
-    it('Renders an input for the workout notes', () => {
-        render(<WorkoutForm />, { wrapper: Wrapper });
-        expect(screen.getByPlaceholderText('Add notes about your workout (optional)')).toBeInTheDocument();
-    });
-
-    it('Renders an input workout duration', () => {
-        render(<WorkoutForm />, { wrapper: Wrapper });
-        expect(screen.getByLabelText('Duration (minutes)')).toBeInTheDocument();
-        expect(screen.getByPlaceholderText('60')).toBeInTheDocument();
-    });
-
-    it('Renders an add exercise button', () => {
-        render(<WorkoutForm />, { wrapper: Wrapper });
-        expect(screen.getByText('Add an Exercise')).toBeInTheDocument();
-    });
-
-    it('Renders a submit button', () => {
-        render(<WorkoutForm />, { wrapper: Wrapper });
-        expect(screen.getByText('Finish')).toBeInTheDocument();
-    });
+    expect(input).toHaveValue("Worked out all night");
+  });
 });
 
-describe('Worokut title input', () => {
-    it("has correct value after user input", () => {
-        render(<WorkoutForm />, { wrapper: Wrapper });
-        const input = screen.getByPlaceholderText('Add a title to your workout');
+describe("Worokut duration input", () => {
+  it("has correct value after user input", () => {
+    render(<WorkoutForm />, { wrapper: Wrapper });
+    const input = screen.getByPlaceholderText("60");
 
-        userEvent.type(input, "Ring Fit");
-    
-        expect(input).toHaveValue("Ring Fit");
-    });
-});
+    userEvent.type(input, "65");
 
-describe('Worokut notes input', () => {
-    it("has correct value after user input", () => {
-        render(<WorkoutForm />, { wrapper: Wrapper });
-        const input = screen.getByPlaceholderText('Add notes about your workout (optional)');
-
-        userEvent.type(input, "Worked out all night");
-    
-        expect(input).toHaveValue("Worked out all night");
-    });
-});
-
-describe('Worokut duration input', () => {
-    it("has correct value after user input", () => {
-        render(<WorkoutForm />, { wrapper: Wrapper });
-        const input = screen.getByPlaceholderText('60');
-
-        userEvent.type(input, "65");
-    
-        expect(input).toHaveValue(65);
-    });
+    expect(input).toHaveValue(65);
+  });
 });
